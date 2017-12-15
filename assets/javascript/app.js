524be047ae5625054780e0659f888318b23372df
kvb <kvb@github.com>
2017-12-14 19:04:44 -0600

    added basic code to index.html and app.js - added reset.css

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// make sure JavaScript doesn't run until the HTML has finished loading
+$(document).ready(function() {
+
+// -------------------- Global Variables --------------------
+
+
+
+// -------------------- Functions and Processes -------------
+
+
+
+// -------------------- HTML --------------------------------
+
+
+
+
+
+
+
+});
