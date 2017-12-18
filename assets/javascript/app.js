a1c581675032817abe52fcac655f1601f9773236
kvb <kvb@github.com>
2017-12-17 20:40:56 -0600

    added some global variables and trivia questions to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,32 @@
 $(document).ready(function() {
 
 // -------------------- Global Variables --------------------
+var questions = [
+    {
+        question: "How many toes does a sloth have on each foot?",
+        answers: {
+            a: "4",
+            b: "7",
+            c: "3",
+            d: "2",
+        },
+            correctAnsw: "c",
+    },
+    {
+        question: "How many times a week do sloths defecate?",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3",
+            d: "4",
+        },
+            correctAnsw: "a",
+    },
+];
 
-
+var correctAnsw;
+var wrongAnsw;
+var unAnsw;
 
 // -------------------- Functions and Processes -------------
 
